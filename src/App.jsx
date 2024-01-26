c1e2d0ee6adcc787a1f29b8b9f1e97e1430b7e7b
PabloZottola <pablozottola66@gmail.com>
2024-01-26 14:59:38 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <main>
+        <section>
+          <ul className="grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] justify-items-center gap-3 text-white"></ul>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+      <>
+        <header>
+          <h1 className="text-5xl font-semibold text-white text-center mt-10 border-b-2 pb-4">Libritos de Midu! 📚</h1>
+        </header>
+      </>
+    );
+  }
+  
+  export default Header;
+  
