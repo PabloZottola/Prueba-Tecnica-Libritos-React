3aa81f047abc516c0cae3e877f4ab44f25130057
Zottola <pablozottola66@gmail.com>
2024-01-29 01:01:56 -0300

    fix responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,20 +12,20 @@ function Home() {
   }
 
   return (
-    <main className="grid grid-cols-6 divide-x">
-      <section className="col-span-4 py-6">
+    <main className="grid tablet:grid-cols-6 tablet:divide-x tablet:divide-y-0 divide-y desktop:w-10/12 laptop:w-3/4 m-auto tablet:pt-1">
+      <section className="tablet:col-span-4 py-5 pr-2">
         <h2 className="text-white text-center text-3xl font-semibold pb-6">
           Lista de Libros
         </h2>
-        <ul className="grid grid-cols-[repeat(auto-fill,_minmax(180px,_1fr))] justify-items-center gap-3 mx-6 text-white">
+        <ul className="grid grid-cols-[repeat(auto-fill,_minmax(180px,_1fr))] justify-items-center gap-2 mx-auto text-white">
           <BookList handleRear={handleRear} />
         </ul>
       </section>
-      <aside className="col-span-2 py-6">
+      <aside className="tablet:col-span-2 py-5 pl-2">
         <h2 className="text-white text-center text-3xl font-semibold pb-6">
           Lista de Lectura
         </h2>
-        <ul className="grid grid-cols-[repeat(auto-fill,_minmax(180px,_1fr))] justify-items-center gap-3 mx-6 text-white">
+        <ul className="grid grid-cols-[repeat(auto-fill,_minmax(180px,_1fr))] justify-items-center gap-2 mx-auto text-white">
           <ReadList />
         </ul>
       </aside>
